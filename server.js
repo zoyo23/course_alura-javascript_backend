fcbc9b8297e911ceba8283204271343c61d1e0d1
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-06-18 08:40:01 -0300

    03 - Encapsulando o Express

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express();
+const app = require('./src/config/custom-express');
 
 app.listen(3000, function () {
     console.log(`Servidor rodando na porta: 3000`)
