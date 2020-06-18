59d5fee996339feb75e9841c4b9b2840b3c14027
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-06-18 08:46:05 -0300

    05 - Encapsulando as rotas

--- server.js
+++ server.js
@@ -3,29 +3,3 @@ const app = require('./src/config/custom-express');
 app.listen(3000, function () {
     console.log(`Servidor rodando na porta: 3000`)
 });
-
-app.get('/', function (req, resp) {
-    resp.send(`
-    <html lang="pt-BR">
-        <head>
-            <meta charset="UTF-8">
-        </head>
-        <body>
-            <h1>Casa do Código</h1>
-        </body>
-    </html>`
-    );
-});
-
-app.get('/livros', function (req, resp) {
-    resp.send(`
-    <html lang="pt-BR">
-        <head>
-            <meta charset="UTF-8">
-        </head>
-        <body>
-            <h1>Listagem de livros</h1> 
-        </body>
-    </html>`
-    );
-});
---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,27 @@
+module.exports = (app) => {
+    app.get('/', function (req, resp) {
+        resp.send(`
+        <html lang="pt-BR">
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <body>
+                <h1>Casa do Código</h1>
+            </body>
+        </html>`
+        );
+    });
+
+    app.get('/livros', function (req, resp) {
+        resp.send(`
+        <html lang="pt-BR">
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <body>
+                <h1>Listagem de livros</h1> 
+            </body>
+        </html>`
+        );
+    });
+}
