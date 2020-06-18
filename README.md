6f75d15618fd412f787a4b7eeed7df538302a86e
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-06-18 08:16:43 -0300

    07 - Organização do projeto

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# [Alura] Nodejs Fundamentos
+# [Alura] Node.js Fundamentos
 # Repositório com projeto desenvolvido durante o curso de fundamentos de NodeJs da Alura. 
 ## Referência: https://cursos.alura.com.br/course/nodejs-fundamentos
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const http = require('http');
 
 const servidor = http.createServer(function (req, resp) {
-    resp.end(`
-    <html lang="pt-BR">
-        <head>
-            <meta charset="UTF-8">
-        </head>
-        <body>
-            <h1>Casa do Código</h1>
-        </body>
-    </html>`);
+    let html = '';
+    if (req.url == '/') {
+        html = `
+        <html lang="pt-BR">
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <body>
+                <h1>Casa do Código</h1>
+            </body>
+        </html>`;
+    }
+    else if (req.url == '/livros') {
+        html = `
+        <html lang="pt-BR">
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <body>
+                <h1>Listagem de livros</h1> 
+            </body>
+        </html>`
+    }
 
+    resp.end(html);
 });
 servidor.listen(3000);
